316d4408134eac8957b2b7c5da35b57dfc6c8e7e
Chris <xathanas@gmail.com>
2020-04-23 21:46:22 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='food_volume_estimation',
+    version='0.1.5',
+    install_requires=[
+        'PyYAML',
+        'opencv-python',
+        'numpy',
+        'matplotlib',
+        'open3d-python',
+        'opencv-contrib-python',
+        'scipy'
+    ]
+)
